09744304f1ab007b5c78f5d31806444ebc71e387
NameOfFool <ivan.pauk2008@gmail.com>
2024-03-21 14:20:58 +0300

    fix token and add command

--- main.js
+++ main.js
@@ -1,12 +1,19 @@
 const TelegramBot = require('node-telegram-bot-api');
-const token = '6332055925:AAFK7xv_415GnnQb6WZyG5S6mrkiWw0sO40';
+require('dotenv').config()
+const token = process.env.TOKEN;
 const bot = new TelegramBot(token, { polling: true });
-bot.on('message', (msg) => {
-  var requests = ["когда эта пара закончится", "когда конец пары?"];
-  var response = getMins() + ' ' + getCase(getMins())
-  if(requests.includes(msg.text.toString().toLowerCase()))
-  bot.sendMessage(msg.chat.id, response); 
+
+bot.on('message', sendTimeMessage)
+//TODO если введено 3 сообщения градация
+
+
+bot.onText(/\/start/, (msg) => {
+
+  bot.sendMessage(msg.chat.id, "Welcome");
+  
 });
+        
+
 function getMins()
 {
   var currentDate = new Date()
@@ -22,12 +29,29 @@ function getMins()
   } 
   return -1;
 }
+
 function getCase(a)
 {
   if(a % 10 == 1 && a % 100 !=11)
     return 'минута'
-  else if(a % 10 >= 2 && a % 10 <=4)
+  else if(a % 10 >= 2 && a % 10 <=4 && (a % 100 <= 10 || a % 100 > 20))
     return 'минуты'
   else
     return 'минут' 
+}
+function sendTimeMessage(msg){
+  var requests = ["когда", "пара", "кончится", "?"]
+  let check = true
+  for (let word of requests){
+    if(msg.text.includes("/left")){
+      break
+    }
+    if(!msg.text.toString().toLowerCase().includes(word)){
+      check = false;
+      break;
+    }
+  }
+  var response = getMins() + ' ' + getCase(getMins())
+  if(check)
+    bot.sendMessage(msg.chat.id, response);
 }
